6c66aa961a1278c57f83d92f9308e25e912b231c
jschang19 <wwwsean13579@gmail.com>
2023-10-30 20:16:45 +0800

    chore: Add max-instance for deployment

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ gcloud functions deploy shawn_cat_line \
 --runtime=nodejs20 \
 --region=asia-east1 \
 --source=. \
+--max-instances=5 \
 --entry-point=main \
 --trigger-http \
---allow-unauthenticated
+--allow-unauthenticated 
 
 echo "Deployed to gcloud!"
