3c7e19b3f0a4e1bd4c64e024605b6ba557167c21
jschang19 <wwwsean13579@gmail.com>
2023-10-31 18:44:39 +0800

    chore: Update deploy command for entry point

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ echo "Deploying to gcloud..."
 
 gcloud functions deploy shawn_cat_line \
 --gen2 \
---runtime=nodejs20 \
+--runtime=nodejs18 \
 --region=asia-east1 \
 --source=. \
---max-instances=5 \
---entry-point=main \
+--max-instances=3 \
+--entry-point=handleRequest \
 --trigger-http \
 --allow-unauthenticated 
 
