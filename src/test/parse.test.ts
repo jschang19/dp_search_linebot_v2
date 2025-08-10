f09f376c25d36c3d54a0f8d7796cb0b07db6b5be
jschang19 <wwwsean13579@gmail.com>
2025-08-10 15:42:20 +0800

    chore: added more test coverage

--- src/test/parse.test.ts
+++ src/test/parse.test.ts
@@ -68,3 +68,73 @@ describe("Test parseSearchTerms with no sepicfic university", () => {
 		expect(actual).toEqual(expected);
 	});
 });
+
+describe("Test parseSearchTerms with national university", () => {
+	it("should return correct search terms", async () => {
+		const test = "國立台灣大學資管";
+		const actual = await parseSearchTerms(test);
+		const expected = {
+			university: "臺灣大學",
+			universityCode: "ntu",
+			major: "資管",
+			searchMode: null,
+		};
+
+		expect(actual).toEqual(expected);
+	});
+});
+
+describe("Test parseSearchTerms with special university cases", () => {
+	it("should correctly identify 中山大學", async () => {
+		const test = "中山資管";
+		const actual = await parseSearchTerms(test);
+		const expected = {
+			university: "中山",
+			universityCode: "nsysu",
+			major: "資管",
+			searchMode: null,
+		};
+
+		expect(actual).toEqual(expected);
+	});
+
+	it("should correctly identify 中山醫學大學", async () => {
+		const test = "中山醫資管";
+		const actual = await parseSearchTerms(test);
+		const expected = {
+			university: "中山醫",
+			universityCode: "csmu",
+			major: "資管",
+			searchMode: null,
+		};
+
+		expect(actual).toEqual(expected);
+	});
+
+
+	it("should correctly identify 中山醫學大學", async () => {
+		const test = "中山醫學大學資管";
+		const actual = await parseSearchTerms(test);
+		const expected = {
+			university: "中山醫學大學",
+			universityCode: "csmu",
+			major: "資管",
+			searchMode: null,
+		};
+
+		expect(actual).toEqual(expected);
+	});
+
+	it("should correctly identify 中山醫學大學 with search mode", async () => {
+		const test = "個申中山醫資管";
+		const actual = await parseSearchTerms(test);
+		const expected = {
+			university: "中山醫",
+			universityCode: "csmu",
+			major: "資管",
+			searchMode: "cac",
+		};
+
+		expect(actual).toEqual(expected);
+	});
+});
